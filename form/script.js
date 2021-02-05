5e78d30bcd491aaff01a911ff8af9e097249f664
JuarezFranca <kjootaa@gmail.com>
2021-02-05 12:08:21 -0300

    first commit

---
+++ form/script.js
@@ -0,0 +1,22 @@
+let B7Validator = {
+    handleSubmit:(event) => {
+        event.preventDefault();
+        let send = true;
+
+        let inputs = form.querySelectorAll('input');
+
+        for(let i = 0; i < inputs.length; i++) {
+            let input = inputs[i];
+            B7Validator.checkInput(input)
+
+        }
+        send = false;
+
+        if(send) {
+            form.submit();
+        }
+    }
+}
+
+let form = document.querySelector('.b7validator');
+form.addEventListener('submit', B7Validator.handleSubmit);
